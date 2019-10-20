04034f26560e2d74ad005530299a4b514484b9f8
Jonas Leal <jonaslabinfo@gmail.com>
2019-10-20 10:14:02 -0300

    Geracao da entidade projeto com scaffold

---
+++ db/migrate/20191019234303_create_projetos.rb
@@ -0,0 +1,13 @@
+class CreateProjetos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :projetos do |t|
+      t.string :titulo
+      t.string :area
+      t.date :dataFin
+      t.date :dataInicio
+      t.string :tipoProjeto
+
+      t.timestamps
+    end
+  end
+end
