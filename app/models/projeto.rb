a2361c53ba3e49f3a316e882ccb14688ddfdc869
Iris <vianasantana21@gmail.com>
2019-10-22 09:04:20 -0300

    adicionando validacoes

--- app/models/projeto.rb
+++ app/models/projeto.rb
@@ -1,2 +1,9 @@
 class Projeto < ApplicationRecord
+  #Validacoes
+  validates :titulo, presence: {message: " nao pode ser em branco" },
+            length: {minimum: 5,too_short: "deve ter pelo menos 5 caracteres" }
+  validates :area, presence: {message: " nao pode ser em branco" },
+            length: {minimum: 5,too_short: "deve ter pelo menos 5 caracteres" }
+  validates :tipoProjeto, presence: {message: " nao pode ser em branco" },
+            length: {minimum: 5,too_short: "deve ter pelo menos 5 caracteres" }
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,8 +1,18 @@
 class Usuario < ApplicationRecord
-  validates :email, presence: true, length: {minimum: 5}
-  validates_confirmation_of :senha, presence: true, length: {minimum: 5}
-  validates :nome, presence: true, length: {minimum: 5}
-  validates :cpf, presence: true, length: {minimum: 11}, :numericality => {:only_integer => true}
-  validates :curso, presence: true, length: {minimum: 5}
-  validates :especificao, presence: true, length: {minimum: 5}
+  #Validacoes
+  validates :nome, presence: { message: "nao pode ser em branco ou conter acentuacao"},
+            length: { minimum: 3, too_short: "deve ter pelo menos 3 caracteres" },
+            format: { with: /\A[a-zA-Z\s]+\z/, message: "nao pode conter caracteres especiais ou numeros" }
+  validates :cpf, presence: { message: "nao pode ser em branco"},
+            length: { is:11, message:"deve ter 11 digitos"},
+            numericality: { only_integer:true,message:"deve conter apenas numeros" }
+  validates :senha, presence: {message: " nao pode ser em branco" },
+            length: {minimum: 6,too_short: "deve ter pelo menos 6 caracteres" }
+  validates :especificao, presence: {message: " nao pode ser em branco" },
+            length: {minimum: 5,too_short: "deve ter pelo menos 5 caracteres" }
+  validates :curso, presence: {message: " nao pode ser em branco" },
+            length: {minimum: 5,too_short: "deve ter pelo menos 5 caracteres" }
+  validates :email, presence: {message: " e-mail nao pode ser em branco" },
+             format: { with: URI::MailTo::EMAIL_REGEXP, message: "apenas permite emails validos" }
+
 end
