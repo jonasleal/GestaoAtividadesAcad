234a26d10c5bcf98a2e062a07e3d1941790f1bb4
LuisF <luisfernando.25@hotmail.com>
2019-10-23 13:23:12 -0300

    Criando test-model criar usuario valido

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -4,4 +4,9 @@ class UsuarioTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'criar usuario valido' do
+    usuario = Usuario.new email: 'jose34@.gmail.com', senha: 'senhaValida12', nome: 'jose matias', cpf: '00000000000', curso: 'BCC', especificao: 'professor'
+    assert usuario.save
+  end
 end
