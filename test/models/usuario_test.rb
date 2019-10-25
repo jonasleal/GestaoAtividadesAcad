a90deedb28d2c273840aa9fb29d903252b77db56
Iris <vianasantana21@gmail.com>
2019-10-24 21:07:30 -0300

    ajeitando testes de model de usuario que tinham falhado

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -6,24 +6,24 @@ class UsuarioTest < ActiveSupport::TestCase
   # end
 
   test 'criar usuario valido' do
-    usuario = Usuario.new email: 'jose34@.gmail.com', senha: 'senhaValida12', nome: 'jose matias', cpf: '00000000000', curso: 'BCC', especificao: 'professor'
+    usuario = Usuario.new email: 'jose12@gmail.com', senha: 'senhaValida12', nome: 'jose matias', cpf: '00000000000', curso: 'BCC', especificao: 'professor'
     assert usuario.save
   end
 
   test 'nao editar usuario com e-mail em branco' do
-    usuario = Usuario.new email: 'jose34@.gmail.com', senha: 'senhaValida12', nome: 'jose matias', cpf: '00000000000', curso: 'BCC', especificao: 'professor'
+    usuario = Usuario.new email: 'jose34@gmail.com', senha: 'senhaValida12', nome: 'jose matias', cpf: '00000000000', curso: 'BCC', especificao: 'professor'
     assert usuario.save
     assert_not usuario.update email: ''
   end
 
   test 'editar usuario com senha valida' do
-    usuario = Usuario.new email: 'jose34@.gmail.com', senha: 'senhaValida12', nome: 'jose matias', cpf: '00000000000', curso: 'BCC', especificao: 'professor'
+    usuario = Usuario.new email: 'jose34@gmail.com', senha: 'senhaValida12', nome: 'jose matias', cpf: '00000000000', curso: 'BCC', especificao: 'professor'
     assert usuario.save
     assert usuario.update senha: 'meuCursoBCC'
   end
 
-  test 'criar usuario invalido' do
-    usuario = Usuario.new email: 'jose34@.gmail.com', senha: 'senhaValida12', nome: 'jose matias@@@@', cpf: '00000000000', curso: 'BCC', especificao: 'professor'
+  test 'nao criar usuario invalido' do
+    usuario = Usuario.new email: 'jose34@gmail.com', senha: 'senhaValida12', nome: 'jose matias@@@@', cpf: '00000000000', curso: 'BCC', especificao: 'professor'
     assert_not usuario.save
   end
 end
