097ec4c5acb3a0937b37b396327ead31a277086b
Iris <vianasantana21@gmail.com>
2019-11-06 13:29:22 -0300

    criando entidade bibliogragia usando scaffold

---
+++ app/views/bibliografia/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! "bibliografia/bibliografium", bibliografium: @bibliografium
---
+++ test/controllers/bibliografia_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class BibliografiaControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @bibliografium = bibliografia(:one)
+  end
+
+  test "should get index" do
+    get bibliografia_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_bibliografium_url
+    assert_response :success
+  end
+
+  test "should create bibliografium" do
+    assert_difference('Bibliografium.count') do
+      post bibliografia_url, params: { bibliografium: { descricao: @bibliografium.descricao, titulo: @bibliografium.titulo } }
+    end
+
+    assert_redirected_to bibliografium_url(Bibliografium.last)
+  end
+
+  test "should show bibliografium" do
+    get bibliografium_url(@bibliografium)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_bibliografium_url(@bibliografium)
+    assert_response :success
+  end
+
+  test "should update bibliografium" do
+    patch bibliografium_url(@bibliografium), params: { bibliografium: { descricao: @bibliografium.descricao, titulo: @bibliografium.titulo } }
+    assert_redirected_to bibliografium_url(@bibliografium)
+  end
+
+  test "should destroy bibliografium" do
+    assert_difference('Bibliografium.count', -1) do
+      delete bibliografium_url(@bibliografium)
+    end
+
+    assert_redirected_to bibliografia_url
+  end
+end
