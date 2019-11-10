7f176e3e3fc6653062705aa45b94bca261451c60
Iris <vianasantana21@gmail.com>
2019-11-10 13:16:52 -0300

    adicionando relacionamento entre bibliografia e projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-
-  resources :bibliografia
-  resources :historico_atividades
-  resources :atividades
   get 'welcome/index'
   resources :projetos do
     resources :trabalhos
     resources :bibliografia
+    resources :atividades
   end
   resources :usuarios
-
+  resources :atividades do
+    resources :historico_atividades
+  end
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
