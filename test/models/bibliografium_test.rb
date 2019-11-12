24c0bb9105a82a5942296725fc21959805c1923c
LuisF <luisfernando.25@hotmail.com>
2019-11-12 20:34:02 -0300

    Teste-model criar bibliografia com descricao sem minimo de caracteres

--- test/models/bibliografium_test.rb
+++ test/models/bibliografium_test.rb
@@ -17,4 +17,11 @@ class BibliografiumTest < ActiveSupport::TestCase
     bibliografia = Bibliografium.new titulo: "", descricao: "teste", projeto_id:projeto.id
     assert_not bibliografia.save
   end
+
+  test 'criar bibliografia com descricao sem minimo de caracteres' do
+    projeto= Projeto.new titulo:"Gestao de projeto", area:"Informatica", dataFin:"20/10/2020", dataInicio:"20/10/2019", tipoProjeto:"tcc"
+    assert projeto.save
+    bibliografia = Bibliografium.new titulo: "teste", descricao: "13", projeto_id:projeto.id
+    assert_not bibliografia.save
+  end
 end
