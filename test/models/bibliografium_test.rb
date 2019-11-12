18d5bf828ebe236b99ceef30492ff0f3ae5d27d8
LuisF <luisfernando.25@hotmail.com>
2019-11-12 20:36:06 -0300

    Teste-model remover bibliografia

--- test/models/bibliografium_test.rb
+++ test/models/bibliografium_test.rb
@@ -24,4 +24,12 @@ class BibliografiumTest < ActiveSupport::TestCase
     bibliografia = Bibliografium.new titulo: "teste", descricao: "13", projeto_id:projeto.id
     assert_not bibliografia.save
   end
+
+  test 'remover bibliografia' do
+    projeto= Projeto.new titulo:"Gestao de projeto", area:"Informatica", dataFin:"20/10/2020", dataInicio:"20/10/2019", tipoProjeto:"tcc"
+    assert projeto.save
+    bibliografia = Bibliografium.new titulo: "teste", descricao: "teste", projeto_id:projeto.id
+    assert bibliografia.save
+    assert bibliografia.destroy
+  end
 end
