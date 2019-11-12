179e277043c25529fda0852fbddb0769ae0b3ac2
LuisF <luisfernando.25@hotmail.com>
2019-11-12 20:31:58 -0300

    Teste-model criar bibliografia valida

--- test/models/bibliografium_test.rb
+++ test/models/bibliografium_test.rb
@@ -4,4 +4,10 @@ class BibliografiumTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'criar bibliografia valida' do
+    projeto= Projeto.new titulo:"Gestao de projeto", area:"Informatica", dataFin:"20/10/2020", dataInicio:"20/10/2019", tipoProjeto:"tcc"
+    assert projeto.save
+    bibliografia = Bibliografium.new titulo: "teste", descricao: "teste", projeto_id:projeto.id
+    assert bibliografia.save
+  end
 end
