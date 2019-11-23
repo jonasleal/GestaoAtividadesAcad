819dcab6ee83a0aa2e7a4e298a2239d7da04608c
Iris <vianasantana21@gmail.com>
2019-11-23 14:26:00 -0300

    alterando arquivos apontados com erros pelo travis

--- app/models/historico_atividade.rb
+++ app/models/historico_atividade.rb
@@ -1,9 +1,6 @@
 class HistoricoAtividade < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :atividade,optional:true
-=======
   belongs_to :atividade
->>>>>>> Criando e editando arquivos ao adicionar associacoes
 end
 
 
