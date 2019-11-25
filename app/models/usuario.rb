d1b176a7fb67b6b9d221b393546f273a0c65c23e
Jonas Leal <jonaslabinfo@gmail.com>
2019-11-25 00:46:44 -0300

    Remoção de especificacao de usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -12,8 +12,6 @@ class Usuario < ApplicationRecord
 
             format: {with: /\A[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}\z/,
                      message: "deve seguir o formato: 000.000.000-00"}
-  validates :especificao, presence: {message: " nao pode ser em branco" },
-            length: {minimum: 3,too_short: "deve ter pelo menos 3 caracteres" }
   validates :curso, presence: {message: " nao pode ser em branco" },
             length: {minimum: 3,too_short: "deve ter pelo menos 3 caracteres" }
 
